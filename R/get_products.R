b8b57836d1b19ee4c6d3822da863e15687c2f2c5
jcrodriguez1989 <jcrodriguez@unc.edu.ar>
2025-01-03 10:30:15 -0300

    Initial commit

---
+++ R/get_products.R
@@ -0,0 +1,47 @@
+#' Get Products
+#'
+#' Receive a list of Products.
+#' Docs: https://tiendanube.github.io/api-documentation/resources/product .
+#'
+#' @param store_id The ID of the store.
+#' @param page Page to retrieve.
+#' @param per_page Amount of results.
+#' @param access_token The store's access token for your app.
+#'
+#' @importFrom httr add_headers content GET
+#'
+#' @export
+#'
+get_products <- function(store_id, page, per_page = 200,
+                         access_token = Sys.getenv("TIENDANUBE_ACCESS_TOKEN")) {
+  GET(
+    paste0(api_url, store_id, "/products"),
+    query = list(per_page = per_page, page = page),
+    add_headers(Authentication = paste("bearer", access_token))
+  ) |> content(as = "parsed", simplifyVector = TRUE)
+}
+
+#' Get All Products
+#'
+#' Receive a list of all Products.
+#'
+#' @param store_id The ID of the store.
+#' @param access_token The store's access token for your app.
+#'
+#' @importFrom dplyr bind_rows
+#'
+#' @export
+#'
+get_all_products <- function(store_id, access_token = Sys.getenv("TIENDANUBE_ACCESS_TOKEN")) {
+  page <- 1
+  products <- data.frame()
+  while (TRUE) {
+    new_products <- get_products(store_id, page, access_token = access_token)
+    if (isTRUE(new_products$code == 404)) {
+      break
+    }
+    products <- bind_rows(new_products, products)
+    page <- page + 1
+  }
+  return(products)
+}
---
+++ R/tiendanube-package.R
@@ -0,0 +1,5 @@
+#' Tienda Nube API interface.
+#'
+"_PACKAGE"
+
+api_url <- Sys.getenv("TIENDANUBE_API_URL", "https://api.tiendanube.com/v1/")
